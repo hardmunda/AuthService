82626ac592fff32f22d6cd9f78b5dbbab10391d3
Rising Soul <hardboy909w@gmail.com>
2024-06-04 23:37:52 +0530

    SignUp logic with Password Encryption done

--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
 const express = require('express');
-const app = express();
+const bodyParser = require('body-parser');
 
 const {PORT} = require('./config/serverConfig');
+const apiRoutes = require('./routes/index');
 
+const app = express();
+// const {User} = require('./models/index');
 
+const prepareAndStartServer = async() => {
+     
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended:true}));
 
-const prepareAndStartServer = () => {
+    app.use('/api',apiRoutes);
 
-    app.listen(3001, () => {
+    app.listen(3001, async () => {
         console.log(`Server started at port: ${PORT}`);
-    })
+    }) 
+    // await User.truncate();
+
 }
 
 prepareAndStartServer();
