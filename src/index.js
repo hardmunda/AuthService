9e82c4baa5d5e3f4ad6cdd0bf0be37381c3db32d
Rising Soul <hardboy909w@gmail.com>
2024-06-05 22:21:54 +0530

    Added roles and user_roles through table

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ const bodyParser = require('body-parser');
 
 const {PORT} = require('./config/serverConfig');
 const apiRoutes = require('./routes/index');
-const UserRepository = require('./repository/user-repository');
+// const db = require('./models/index');
+const {User,Role} = require('./models/index');
 
 const app = express();
-const UserService = require('./services/user-service');
+
 
 const prepareAndStartServer = async() => {
      
@@ -18,13 +19,11 @@ const prepareAndStartServer = async() => {
     app.listen(3001, async () => {
         console.log(`Server started at port: ${PORT}`);
     }) 
-    const user = new UserService;
-    // const newToken = user.createToken({email: 'sanket@admin.com', id: 1});
-    // console.log("new token is", newToken);
-    // const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InNhbmtldEBhZG1pbi5jb20iLCJpZCI6MSwiaWF0IjoxNzE3NTg1MzYwLCJleHAiOjE3MTc1ODUzOTB9.3Piu9wY5d-qR9rOYs3AzpP_Ho_qeHONevZl8itdp-m0';
-    // const response = user.verifyToken(token);
-    // console.log(response);
-
+    if(process.env.DB_SYNC) {
+        db.sequelize.sync({alter: true})
+    }
+    
+  
 }
 
 prepareAndStartServer();
--- src/repository/user-repository.js
+++ src/repository/user-repository.js
@@ -1,4 +1,4 @@
-const {User} = require('../models/index');
+const { User, Role } = require('../models/index');
 
 class UserRepository {
     async create(data) {
@@ -54,6 +54,8 @@ class UserRepository {
 
         }
     }
+
+    
 }
  
 module.exports = UserRepository;
